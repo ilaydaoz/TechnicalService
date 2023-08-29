2afde62eccbaedfb411997977dc197e4d1efde4d
ilayda <ilaydaozken@gmail.com>
2023-08-29 17:52:26 +0300

    Partiallar

---
+++ TechnicalServiceUI/ViewComponents/_Description.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechnicalServiceUI.ViewComponents
+{
+    public class _Description : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ TechnicalServiceUI/ViewComponents/_FooterPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechnicalServiceUI.ViewComponents
+{
+    public class _FooterPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
