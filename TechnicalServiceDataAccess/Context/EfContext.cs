a664774e0f306532fe11adee9dd0709a5553ab6b
ilayda <ilaydaozken@gmail.com>
2023-08-29 18:20:59 +0300

    ServiceRegistration

---
+++ TechnicalServiceDataAccess/Context/EfContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalServiceEntity.Entity;
+
+namespace TechnicalServiceDataAccess.Context
+{
+    public class EfContext : DbContext
+    {
+        public EfContext(DbContextOptions<EfContext> options) : base(options)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<ServiceStatus> ServiceStatuses { get; set; }
+        public DbSet<Technician> Technicians { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasPostgresExtension("uuid-ossp").Entity<Customer>().Property(a => a.Id).HasDefaultValueSql("uuid_generate_v4()").ValueGeneratedOnAdd();
+            modelBuilder.HasPostgresExtension("uuid-ossp").Entity<Device>().Property(a => a.Id).HasDefaultValueSql("uuid_generate_v4()").ValueGeneratedOnAdd();
+            modelBuilder.HasPostgresExtension("uuid-ossp").Entity<Service>().Property(a => a.Id).HasDefaultValueSql("uuid_generate_v4()").ValueGeneratedOnAdd();
+            modelBuilder.HasPostgresExtension("uuid-ossp").Entity<ServiceStatus>().Property(a => a.Id).HasDefaultValueSql("uuid_generate_v4()").ValueGeneratedOnAdd();
+            modelBuilder.HasPostgresExtension("uuid-ossp").Entity<Technician>().Property(a => a.Id).HasDefaultValueSql("uuid_generate_v4()").ValueGeneratedOnAdd();
+        }
+    }
+}
---
+++ TechnicalServiceDataAccess/ServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using TechnicalServiceDataAccess.Context;
+
+namespace TechnicalServiceDataAccess
+{
+    public static class ServiceRegistration
+    {
+        //static sınıfın bir örneğini oluşturmadan doğrudan sınıfın içeriğine erişebilmek için 
+        public static void AddDataAccessServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<EfContext>(x =>
+            {
+                x.UseNpgsql(configuration.GetConnectionString("EfContext"));
+            });
+            services.TryAddScoped<DbContext, EfContext>();
+        }
+    }
+}
